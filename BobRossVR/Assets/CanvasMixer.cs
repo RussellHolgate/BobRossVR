4e125cb5ca27e671b790982f7051172c587cdf06
RussellHolgate <russell.holgate@gmail.com>
2019-11-13 22:38:34 -0500

    canvas transfer update

---
+++ BobRossVR/Assets/CanvasMixer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasMixer : MonoBehaviour
+{
+	public RenderTexture srcTex;
+	public ComputeShader shader;
+
+	RenderTexture dstTex;
+	Renderer renderer_;
+
+	int kernelID;
+
+	void Start()
+	{
+		var width = srcTex.width;
+		var height = srcTex.height;
+		var depth = 24;
+		dstTex = new RenderTexture(width, height, depth);
+		dstTex.enableRandomWrite = true;
+		dstTex.Create();
+
+		kernelID = shader.FindKernel("CSMain");
+		shader.SetTexture(kernelID, "srcTex", srcTex);
+		shader.SetTexture(kernelID, "dstTex", dstTex);
+
+		renderer_ = GetComponent<Renderer>();
+		renderer_.enabled = true;
+	}
+
+	void Update()
+	{
+		UpdateShader();
+	}
+
+	void UpdateShader()
+	{
+		var xThreads = dstTex.width / 8;
+		var yThreads = dstTex.height / 8;
+		var zThreads = 1;
+		shader.Dispatch(kernelID,
+			xThreads, yThreads, zThreads);
+		renderer_.material.SetTexture("_MainTex", dstTex);
+	}
+}
---
+++ BobRossVR/Assets/DisableColliders.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableColliders : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		var comps = GetComponentsInChildren<Collider>();
+		foreach(var comp in comps)
+		{
+			comp.enabled = false;
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
