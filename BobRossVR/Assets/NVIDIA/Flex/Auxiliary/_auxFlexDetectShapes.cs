0d73c9b6e29a228441e8a9a66adc734b566f7eba
RussellHolgate <russell.holgate@gmail.com>
2019-11-29 09:12:50 -0500

    Revert "improved fluid behaviours"
    This reverts commit 260c10f103ed31939ee046f1b765841b3edcd506.

--- BobRossVR/Assets/NVIDIA/Flex/Auxiliary/_auxFlexDetectShapes.cs
+++ BobRossVR/Assets/NVIDIA/Flex/Auxiliary/_auxFlexDetectShapes.cs
@@ -92,25 +92,6 @@ namespace NVIDIA.Flex
 			}
 		}
 
-		//***
-		void cpu_fluid_collisions(ref Collider collider, ref FlexContainer.ParticleData _particleData, float pull_strength, float tolerance = 0.3f)
-		{
-			var fluidIndices = _particleData.container.fluidIndices;
-
-			foreach (var i in fluidIndices)
-			{
-				var position = (Vector3)_particleData.GetParticle(i);
-
-				var closest = collider.ClosestPoint(position);
-
-				if (Vector3.SqrMagnitude(position - closest) < tolerance)
-				{
-					var velocity = _particleData.GetVelocity(i);
-					_particleData.SetVelocity(i, velocity + (closest - position) * pull_strength);
-				}
-			}
-		}
-
 		#region Messages
 
 		void OnEnable()
@@ -151,19 +132,15 @@ namespace NVIDIA.Flex
             {
                 Collider collider = item.Key;
 
-				if (collider.gameObject.tag == "mildly sticky")
+				if (collider is SphereCollider)
 				{
-					cpu_fluid_collisions(ref collider, ref _particleData, 5.0f);
+					doUpdateParticles(_particleData, collider);
 				}
-				else if (collider.gameObject.tag == "sticky")
+				else if (collider.name == "Canvas")
 				{
-					cpu_fluid_collisions(ref collider, ref _particleData, 10.0f);
+					onCanvasCollision(_particleData, collider);
 				}
-				else if (collider.gameObject.tag == "very sticky")
-				{
-					cpu_fluid_collisions(ref collider, ref _particleData, 20.0f);
-				}
-				else if (/*!(collider is SphereCollider) && */!(collider is CapsuleCollider) && !(collider is BoxCollider) && !(collider is MeshCollider))
+                else if (/*!(collider is SphereCollider) && */!(collider is CapsuleCollider) && !(collider is BoxCollider) && !(collider is MeshCollider))
                     continue;
 
                 ++colliderCount;
